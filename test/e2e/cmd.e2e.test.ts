2c89589c91c9d20315610d23cfab1a8c8ca6a4b8
Manan Jethwani <50267986+MananJethwani@users.noreply.github.com>
2021-01-26 00:24:49 +0530

    Update cmd.e2e.test.ts

--- test/e2e/cmd.e2e.test.ts
+++ test/e2e/cmd.e2e.test.ts
@@ -13,7 +13,7 @@ test('Exec Command With Bash', async (t) => {
     const version = await execa.command(`${mwo} --version`);
     t.equal(version.stdout.trim(), packageJSON.version);
 
-//     const help = await execa.command(`${mwo} --help`);
+    const help = await execa.command(`${mwo} --help`);
 //     t.ok(help.stdout.trim().split('\n').length > 55);
 
     // TODO: Consider executing more e2e tests this way
